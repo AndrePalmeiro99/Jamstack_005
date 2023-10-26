765a01c1c93a5f9d4b399e67a39d94df62d45499
André Palmeiro <andrepalmeiro14@gmail.com>
2023-10-26 17:28:55 -0300

    Aplicando layout e CSS

---
+++ src/pages/pagina2.js
@@ -0,0 +1,18 @@
+import * as React from "react"
+import Layout from "../components/layout"
+
+const Pagina2 = () => {
+    return (
+        <Layout>
+            <h1>Essa é uma página 2</h1>
+            <div>
+                <p>Esse é o primeiro parágrafo</p>
+                <p>Esse é o segundo parágrafo</p>
+            </div>
+        </Layout>
+    )
+}
+
+export default Pagina2
+
+export const Head = () => <title>Página 2</title>
